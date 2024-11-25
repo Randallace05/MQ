f713152480046a1052c03c7aa8ab720858b420aa
daryllcadua <mackucadua@gmail.com>
2024-11-25 22:49:05 +0800

    commit

---
+++ distributor_page/includesDistrib/sidebar.php
@@ -0,0 +1,65 @@
+<head></head>
+
+
+
+<style>
+    .bg-color {
+        background-color: #d24444 !important; /* Override the background color */
+        background-image: none !important;
+    }
+</style>
+
+<!-- Sidebar -->
+        <ul class="bg-color navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+            <!-- Sidebar - Brand -->
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="../dashboard/index.php">
+                <div class="sidebar-brand-icon rotate-n-15">
+                    <i class="fa-solid fa-pepper-hot"></i>
+                </div>
+                <div class="sidebar-brand-text mx-3">MQ KITCHEN</div>
+            </a>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider my-0">
+            <!-- Divider -->
+            <!-- <hr class="sidebar-divider"> -->
+
+            <!-- Heading -->
+            <div class="sidebar-heading">
+                Menu
+            </div>
+
+            <!-- Nav Item - Chat -->
+            <li class="nav-item">
+                <a class="nav-link" href="../chat/chat.php">
+                    <i class="fa-solid fa-message"></i>
+                    <span>Chat</span></a>
+            </li>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider">
+
+            <!-- Heading -->
+            <div class="sidebar-heading">
+                Others
+            </div>
+
+            <!-- Nav Item - Users -->
+            <li class="nav-item">
+                <a class="nav-link" href="../settings/settings.php">
+                    <i class="fa-solid fa-gear"></i>
+                    <span>Settings</span></a>
+            </li>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider d-none d-md-block">
+
+            <!-- Sidebar Toggler (Sidebar) -->
+            <div class="text-center d-none d-md-inline">
+                <button class="rounded-circle border-0" id="sidebarToggle"></button>
+            </div>
+        </ul>
+        <!-- End of Sidebar -->
+
+

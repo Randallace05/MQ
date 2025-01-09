602929d3fba4bb3c3ff47e976834e985d37a753f
zaeddd <zaedalvarico@gmail.com>
2025-01-10 02:51:37 +0800

    WAR IS OVER!

--- admin_page/chat/chat.php
+++ admin_page/chat/chat.php
@@ -3,7 +3,6 @@
   include_once "php/config.php";
   if(!isset($_SESSION['unique_id'])){
     header("location: login.php");
-    exit();
   }
 ?>
 <?php include_once "header.php"; ?>
@@ -12,41 +11,26 @@
     <section class="chat-area">
       <header>
         <?php 
-          if(isset($_GET['user_id'])) {
-            $user_id = mysqli_real_escape_string($conn, $_GET['user_id']);
-            $sql = "SELECT * FROM tbl_user WHERE unique_id = ?";
-            $stmt = mysqli_prepare($conn, $sql);
-            if ($stmt) {
-              mysqli_stmt_bind_param($stmt, "s", $user_id);
-              mysqli_stmt_execute($stmt);
-              $result = mysqli_stmt_get_result($stmt);
-              if(mysqli_num_rows($result) > 0){
-                $user = mysqli_fetch_assoc($result);
-              } else {
-                echo "User not found.";
-                exit();
-              }
-              mysqli_stmt_close($stmt);
-            } else {
-              header("location: users.php");
-            }
-          } else {
-            echo "No user selected. Please go back and select a user to chat with.";
-            exit();
+          $user_id = mysqli_real_escape_string($conn, $_GET['user_id']);
+          $sql = mysqli_query($conn, "SELECT * FROM tbl_user WHERE unique_id = {$user_id}");
+          if(mysqli_num_rows($sql) > 0){
+            $row = mysqli_fetch_assoc($sql);
+          }else{
+            header("location: users.php");
           }
         ?>
         <a href="users.php" class="back-icon"><i class="fas fa-arrow-left"></i></a>
-        <img src="php/images/<?php echo htmlspecialchars($user['img']); ?>" alt="">
+        <img src="php/images/<?php echo $row['img']; ?>" alt="">
         <div class="details">
-          <span><?php echo htmlspecialchars($user['first_name'] . " " . $user['last_name']); ?></span>
-          <p><?php echo htmlspecialchars($user['status']); ?></p>
+          <span><?php echo $row['first_name']. " " . $row['last_name'] ?></span>
+          <p><?php echo $row['status']; ?></p>
         </div>
       </header>
       <div class="chat-box">
-        <!-- Chat messages will be loaded here dynamically -->
+
       </div>
       <form action="#" class="typing-area">
-        <input type="text" class="incoming_id" name="incoming_id" value="<?php echo htmlspecialchars($user_id); ?>" hidden>
+        <input type="text" class="incoming_id" name="incoming_id" value="<?php echo $user_id; ?>" hidden>
         <input type="text" name="message" class="input-field" placeholder="Type a message here..." autocomplete="off">
         <button><i class="fab fa-telegram-plane"></i></button>
       </form>
@@ -57,4 +41,3 @@
 
 </body>
 </html>
-

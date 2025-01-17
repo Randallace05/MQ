e7ab58ffbd74773fb796a4e14389916e076a420e
Randallace05 <randallace.balanay@cvsu.edu.ph>
2025-01-18 00:18:50 +0800

    Transaction History

--- user_page/cart.php
+++ user_page/cart.php
@@ -14,7 +14,7 @@ require_once '../endpoint/session_config.php';
     <div class="container">
         <div class="flex-container">
             <!-- Image Section -->
-            <?php include("cartZ.php"); ?>
+            <?php include("cartItems.php"); ?>
             <!-- Form Section -->
             <div class="main">
 <script>
